3b43ec2fb988569b278dada2ccabc745981057cc
chigomuh <tngusrldnjs1@gmail.com>
2023-02-23 02:43:23 +0900

    refactor: CommunityContainer를 리팩토링합니다

--- components/views/Home/Community/MovieReview/MovieReviewContainer.tsx
+++ components/views/Home/Community/MovieReview/MovieReviewContainer.tsx
@@ -1,23 +1,53 @@
 import { useFetchReviews } from '@/services/review'
-import { flexGap } from '@/styles/emotion'
-import { css } from '@emotion/react'
+import { flexGap, flexCenter, mediaQuery } from '@/styles/emotion'
 import MovieReview from './MovieReview'
+import styled from '@emotion/styled'
 
 const MovieReviewContainer = () => {
   const { data: reviews } = useFetchReviews()
+  // 이 부분은 test용 입니다.
+  // 모바일에서 5개보다 적거나
+  // 태블릿이상에서 10개보다 적을 경우의 디자인이 없어서
+  // 임의로 10개 이상이라고 가정합니다.
+  const reviewsMobile = [...reviews, ...reviews, ...reviews].slice(0, 5)
+  const reviewsOthers = [...reviews, ...reviews, ...reviews].slice(5, 10)
 
   return (
-    <div
-      css={css`
-        width: 100%;
-        ${flexGap('1.5rem')}
-      `}
-    >
-      {reviews.map((review) => {
-        return <MovieReview key={review.id} {...review} />
-      })}
-    </div>
+    <Center>
+      <Box>
+        {reviewsMobile.map((review, index) => {
+          return <MovieReview key={`review-${review.id}${index}`} {...review} />
+        })}
+      </Box>
+      <MobileHiddenBox>
+        {reviewsOthers.map((review, index) => {
+          return <MovieReview key={`review-${review.id}${index}`} {...review} />
+        })}
+      </MobileHiddenBox>
+    </Center>
   )
 }
+
+const MobileHiddenBox = styled.div`
+  display: none;
+
+  ${mediaQuery.tablet`
+    width: 50%;
+    ${flexGap('1.5rem')}
+  `}
+`
+
+const Box = styled.div`
+  width: 50%;
+  ${flexGap('1.5rem')}
+`
+
+const Center = styled.div`
+  width: 100%;
+
+  ${mediaQuery.tablet`
+    ${flexCenter}
+  `}
+`
 
 export default MovieReviewContainer
--- components/views/Home/Community/Recent/RecentContainer.tsx
+++ components/views/Home/Community/Recent/RecentContainer.tsx
@@ -1,23 +1,53 @@
 import { useFetchRecents } from '@/services/recent'
-import { flexGap } from '@/styles/emotion'
-import { css } from '@emotion/react'
+import { flexGap, flexCenter, mediaQuery } from '@/styles/emotion'
+import styled from '@emotion/styled'
 import Recent from './Recent'
 
 const RecentContainer = () => {
   const { data: recents } = useFetchRecents()
+  // 이 부분은 test용 입니다.
+  // 모바일에서 5개보다 적거나
+  // 태블릿이상에서 10개보다 적을 경우의 디자인이 없어서
+  // 임의로 10개 이상이라고 가정합니다.
+  const recentsMobile = [...recents, ...recents, ...recents].slice(0, 5)
+  const recentsOthers = [...recents, ...recents, ...recents].slice(5, 10)
 
   return (
-    <div
-      css={css`
-        width: 100%;
-        ${flexGap('1.5rem')}
-      `}
-    >
-      {recents.map((recent) => {
-        return <Recent key={recent.id} {...recent} />
-      })}
-    </div>
+    <Center>
+      <Box>
+        {recentsMobile.map((recent, index) => {
+          return <Recent key={`recent-${recent.id}${index}`} {...recent} />
+        })}
+      </Box>
+      <MobileHiddenBox>
+        {recentsOthers.map((recent, index) => {
+          return <Recent key={`recent-${recent.id}${index}`} {...recent} />
+        })}
+      </MobileHiddenBox>
+    </Center>
   )
 }
+
+const MobileHiddenBox = styled.div`
+  display: none;
+
+  ${mediaQuery.tablet`
+    width: 50%;
+    ${flexGap('1.5rem')}
+  `}
+`
+
+const Box = styled.div`
+  width: 50%;
+  ${flexGap('1.5rem')}
+`
+
+const Center = styled.div`
+  width: 100%;
+
+  ${mediaQuery.tablet`
+    ${flexCenter}
+  `}
+`
 
 export default RecentContainer
