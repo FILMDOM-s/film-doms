5d3c136a105a6e1e43914f88b65136b9d8428ac1
chigomuh <tngusrldnjs1@gmail.com>
2023-02-27 00:22:03 +0900

    fix: 메인 페이지 섹션 간격을 추가합니다

--- components/views/Home/HomeContainer.tsx
+++ components/views/Home/HomeContainer.tsx
@@ -1,13 +1,31 @@
+import { flexGap, mediaQuery } from '@/styles/emotion'
+import styled from '@emotion/styled'
 import CommunityContainer from './Community'
 import CriticConatiner from './Critic/CriticConatiner'
 
 const HomeContainer = () => {
   return (
-    <div>
+    <PageContainer>
       <CommunityContainer />
       <CriticConatiner />
-    </div>
+    </PageContainer>
   )
 }
+
+const PageContainer = styled.div`
+  ${flexGap('64px')}
+
+  ${mediaQuery.tablet`
+    ${flexGap('72px')}
+  `}
+
+  ${mediaQuery.laptop`
+    ${flexGap('80px')}
+  `}
+
+  ${mediaQuery.pc`
+    ${flexGap('120px')}
+  `}
+`
 
 export default HomeContainer
