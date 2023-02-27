dc3924347e9fc47fa99016d395dfa9252df74576
thewoowon <thewoowon@naver.com>
2023-02-27 16:33:25 +0900

    FILMDOMS delete: 초기 컴포넌트 삭제, 이미지 삭제

--- components/FormError.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-interface IFormErrorProps {
-  errorMessage?: string
-}
-
-export function FormError({ errorMessage }: IFormErrorProps) {
-  return <span role={'alert'}>{errorMessage}</span>
-}
