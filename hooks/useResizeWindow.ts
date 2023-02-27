964d27683879e6410ea02877964a4b7e2e3cf66f
thewoowon <thewoowon@naver.com>
2023-02-27 16:36:52 +0900

    FILMDOMS feat: 헤어 로고 이미지 추가, 리사이즈 커스텀 훅 추가

---
+++ hooks/useResizeWindow.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"
+
+const useResizeWindow = () =>{
+
+    const [windowWidth, setWindowWidth] = useState(0)
+    // resize 될때만 함수 불러오기
+    let timer: string | number | NodeJS.Timeout
+    const resizeWindow = () => {
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+          // 현재 window width 값
+          setWindowWidth(window.innerWidth)
+        }, 300)
+      }
+      useEffect(() => {
+        setWindowWidth(window.innerWidth)
+        window.addEventListener('resize', resizeWindow)
+        return () => {
+          window.removeEventListener('resize', resizeWindow)
+        }
+      }, [windowWidth])
+
+      return windowWidth;
+}
+
+export default useResizeWindow;
