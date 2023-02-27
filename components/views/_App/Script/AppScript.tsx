61aa40bd5666c26d180c9d269e754fc7e77880e7
chigomuh <tngusrldnjs1@gmail.com>
2023-02-27 20:51:51 +0900

    feat: AppScript 컴포넌트를 구성합니다

---
+++ components/views/_App/Script/AppScript.tsx
@@ -0,0 +1,13 @@
+import GtagScript from './Gtag'
+import NaverPay from './NaverPay'
+
+const AppScript = () => {
+  return (
+    <>
+      <NaverPay />
+      <GtagScript />
+    </>
+  )
+}
+
+export default AppScript
