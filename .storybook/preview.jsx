03b4327c563e1f979a30a39e039eaa2b4080a57c
chigomuh <tngusrldnjs1@gmail.com>
2023-03-06 02:16:06 +0900

    feat: storybook global style을 적용합니다

---
+++ .storybook/preview.jsx
@@ -0,0 +1,21 @@
+import '../styles/globals.css'
+import GlobalStyles from '@/styles/GlobalStyles'
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+export const decorators = [
+  Story => (
+    <>
+      <GlobalStyles />
+      <Story />
+    </>
+  ),
+]
