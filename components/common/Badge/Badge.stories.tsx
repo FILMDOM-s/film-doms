8fb0c418a2357144873176ac059fa931ec9df3f4
chigomuh <tngusrldnjs1@gmail.com>
2023-03-06 23:22:06 +0900

    feat: common story를 추가합니다

---
+++ components/common/Badge/Badge.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import Badge from './Badge'
+
+export default {
+  component: Badge,
+  title: 'Common/Badge',
+} as ComponentMeta<typeof Badge>
+
+export const Default: ComponentStory<typeof Badge> = args => {
+  return <Badge {...args}>배지</Badge>
+}
+Default.args = {
+  theme: 'default',
+}
