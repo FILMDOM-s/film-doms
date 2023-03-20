4e6959db952e8bedf82d1b9029c4955858363cb4
chigomuh <tngusrldnjs1@gmail.com>
2023-03-20 21:29:51 +0900

    chore: story 경로를 수정합니다

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,10 @@ const path = require('path')
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 module.exports = {
-  // components, pages 폴더 내부만 추가합니다.
+  // stories에 작성된 경로의 story가 추가됩니다.
   stories: [
-    '../(components|pages|assets/svgs)/**/*.stories.mdx',
-    '../(components|pages|assets/svgs)/**/*.stories.@(js|jsx|ts|tsx)',
+    '../(components|pages|assets/svgs|styles/emotion)/**/*.stories.mdx',
+    '../(components|pages|assets/svgs|styles/emotion)/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-links',
