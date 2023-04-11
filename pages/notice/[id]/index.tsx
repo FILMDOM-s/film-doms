0310cbd6d7cc2a935dc91197cc400c8632a3a047
thewoowon <thewoowon@naver.com>
2023-04-11 23:40:51 +0900

    reafacor: notice 섹션 리팩토링

--- pages/notice/[id]/index.tsx
+++ pages/notice/[id]/index.tsx
@@ -1,7 +1,6 @@
-import { useSession } from 'next-auth/react'
+import { OpenGraph } from '@/components/common'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
 
 export default function Index() {
   const router = useRouter()
@@ -10,23 +9,8 @@ export default function Index() {
   return (
     <div>
       <Head>
-        <title>필름덤즈 | NOTICE</title>
-        <meta
-          property="og:url"
-          content={`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/temp`}
-        />
-        <meta property="og:type" content="article" />
-        <meta property="og:title" content="필름덤즈 TEMP" />
-        <meta
-          property="og:description"
-          content="여기에 필름 덤즈 오픈그라프 설명을 입력해주세요."
-        />
-        <meta
-          property="og:image"
-          content="https://imagedelivery.net/6qzLODAqs2g1LZbVYqtuQw/77d77289-00a6-45d3-22be-2366f20eaa00/public"
-        />
+        <OpenGraph/>
       </Head>
-
       <main className="mx-auto">{noticeId}</main>
     </div>
   )
