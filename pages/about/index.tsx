83d29381783f2110b61ba7727ae1a70a20e72b10
thewoowon <thewoowon@naver.com>
2023-04-11 23:41:18 +0900

    feat: 어바웃 작업

---
+++ pages/about/index.tsx
@@ -0,0 +1,21 @@
+import { OpenGraph } from '@/components/common'
+import Head from 'next/head'
+
+const openGraphProps = {
+  title: 'ABOUT',
+  url: 'https://film-doms.vercel.app/',
+  path: 'about',
+  image:
+    'https://imagedelivery.net/6qzLODAqs2g1LZbVYqtuQw/12d3ceb7-c251-4695-144f-ba0338ec6400/public',
+}
+
+export default function Index() {
+  return (
+    <div>
+      <OpenGraph {...openGraphProps} />
+      <main className="mx-auto">
+        About
+      </main>
+    </div>
+  )
+}
