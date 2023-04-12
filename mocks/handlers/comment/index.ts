f3ea9bb3a365dcf2fd24699d548ee3abfcbdc4bb
thewoowon <thewoowon@naver.com>
2023-04-12 22:25:50 +0900

    fix: 빌드 체크, 타입 수정

--- mocks/handlers/comment/index.ts
+++ mocks/handlers/comment/index.ts
@@ -16,7 +16,7 @@ const extractComment = (extract: number) => {
         'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
     },
     childComments: Array.from(
-      new Array<ChildComment>(extract).keys(),
+      new Array<Comment.Child>(extract).keys(),
       (_, i) => ({
         id: i + 1,
         content:
--- services/queryKeys.ts
+++ services/queryKeys.ts
@@ -8,7 +8,7 @@ const queryKeys = {
   recent: (id: Recent['id']) => ['recent', id],
 
   comments: ['comments'],
-  comment: (id: Comment['id']) => ['comment', id],
+  comment: (id: Comment.Parent['id']) => ['comment', id],
 
   notices: ['notices'],
   notice: (id: Notice['id']) => ['notice', id],
