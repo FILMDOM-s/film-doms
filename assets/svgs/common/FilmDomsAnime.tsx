dc2cd9faa7581d5c4988705db844109805422693
thewoowon <thewoowon@naver.com>
2023-04-16 14:30:35 +0900

    feat: about 관련 작업물

---
+++ assets/svgs/common/FilmDomsAnime.tsx
@@ -0,0 +1,179 @@
+const FilmDomsAnime = () => {
+  return (
+    <svg
+      width="1281"
+      height="626"
+      viewBox="0 0 1281 626"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="letters letters--effect-1"
+    >
+      {/* F */}
+      <g className="letter letter--1">
+        <g className="letter__part">
+          <path
+            className="letter__layer color-1"
+            d="M295.385 78.8228V0H164.103H0V78.8228V131.371V210.194V295.586H164.103V210.194H262.564V131.371H164.103V78.8228H295.385Z"
+            fill="#F7F7F5"
+          />
+        </g>
+        <g className="letter__part">
+          <path
+            className="letter__layer color-3"
+            d="M295.385 78.8228V0H164.103H0V78.8228V131.371V210.194V295.586H164.103V210.194H262.564V131.371H164.103V78.8228H295.385Z"
+            fill="#F7F7F5"
+          />
+        </g>
+        <g className="letter__part">
+          <path className="letter__layer color-2" 
+          d="M295.385 78.8228V0H164.103H0V78.8228V131.371V210.194V295.586H164.103V210.194H262.564V131.371H164.103V78.8228H295.385Z" fill="#F7F7F5"
+          />
+        </g>
+      </g>
+      {/* I */}
+      <g className="letter letter--2">
+        <g className="letter__part">
+          <path
+            className="letter__layer color-1"
+            d="M623.592 78.8228V0H574.361H377.438H328.207V78.8228H377.438V216.763H328.207V295.586H377.438H574.361H623.592V216.763H574.361V78.8228H623.592Z"
+            fill="#E96437"
+          />
+        </g>
+        <g className="letter__part">
+          <path
+            className="letter__layer color-3"
+            d="M623.592 78.8228V0H574.361H377.438H328.207V78.8228H377.438V216.763H328.207V295.586H377.438H574.361H623.592V216.763H574.361V78.8228H623.592Z"
+            fill="#E96437"
+          />
+        </g>
+        <g className="letter__part">
+          <path
+            className="letter__layer color-2"
+            d="M623.592 78.8228V0H574.361H377.438H328.207V78.8228H377.438V216.763H328.207V295.586H377.438H574.361H623.592V216.763H574.361V78.8228H623.592Z"
+            fill="#E96437"
+          />
+        </g>
+      </g>
+      {/* L */}
+      <g className="letter letter--3">
+        <g className="letter__part">
+          <path className="letter__layer color-1" 
+          d="M820.509 164.214V0H656.406V164.214V295.586H820.509H951.791V164.214H820.509Z" fill="#FFEB74"
+          />
+        </g>
+        <g className="letter__part">
+          <path className="letter__layer color-3"
+          d="M820.509 164.214V0H656.406V164.214V295.586H820.509H951.791V164.214H820.509Z" fill="#FFEB74"
+           />
+        </g>
+        <g className="letter__part">
+          <path className="letter__layer color-2" 
+          d="M820.509 164.214V0H656.406V164.214V295.586H820.509H951.791V164.214H820.509Z" fill="#FFEB74"
+          />
+        </g>
+      </g>
+
+      {/* M */}
+      <g className="letter letter--4">
+        <g className="letter__part">
+          <path className="letter__layer color-1" 
+          d="M1184.08 0L1133.24 65.8663L1081.06 0H984.617V295.586H1050.26H1050.72V196.959L1083.42 295.586H1181.64L1214.34 196.959V295.586H1214.36H1280V0H1184.08Z" fill="#E75B4E"/>
+        </g>
+        <g className="letter__part">
+          <path className="letter__layer color-3" 
+          d="M1184.08 0L1133.24 65.8663L1081.06 0H984.617V295.586H1050.26H1050.72V196.959L1083.42 295.586H1181.64L1214.34 196.959V295.586H1214.36H1280V0H1184.08Z" fill="#E75B4E"/>
+        </g>
+        <g className="letter__part">
+          <path className="letter__layer color-2" 
+          d="M1184.08 0L1133.24 65.8663L1081.06 0H984.617V295.586H1050.26H1050.72V196.959L1083.42 295.586H1181.64L1214.34 196.959V295.586H1214.36H1280V0H1184.08Z" fill="#E75B4E"/>
+        </g>
+      </g>
+
+      {/* D */}
+
+      <g className="letter letter--5">
+        <g className="letter__part">
+          <path className="letter__layer color-1" 
+          d="M143.294 330.07H131.282H0V625.656H88.6154H111.59H143.294C227.167 625.656 295.385 559.363 295.385 477.863C295.385 396.364 227.151 330.07 143.294 330.07ZM119.795 527.127V428.599H152.615V527.127H119.795Z" fill="#A0B8FF"/>
+        </g>
+        <g className="letter__part">
+          <path className="letter__layer color-3" 
+          d="M143.294 330.07H131.282H0V625.656H88.6154H111.59H143.294C227.167 625.656 295.385 559.363 295.385 477.863C295.385 396.364 227.151 330.07 143.294 330.07ZM119.795 527.127V428.599H152.615V527.127H119.795Z" fill="#A0B8FF"/>
+        </g>
+        <g className="letter__part">
+          <path className="letter__layer color-2" 
+          d="M143.294 330.07H131.282H0V625.656H88.6154H111.59H143.294C227.167 625.656 295.385 559.363 295.385 477.863C295.385 396.364 227.151 330.07 143.294 330.07ZM119.795 527.127V428.599H152.615V527.127H119.795Z" fill="#A0B8FF"/>
+        </g>
+      </g>
+
+      {/* O */}
+
+      <g className="letter letter--6">
+        <g className="letter__part">
+          <path className="letter__layer color-1" 
+          d="M328.207 328.428V624.013H623.592V328.428H328.207ZM493.951 527.127H461.13V428.598H493.951V527.127Z" fill="#F7F7F5"/>
+        </g>
+        <g className="letter__part">
+          <path className="letter__layer color-3" 
+          d="M328.207 328.428V624.013H623.592V328.428H328.207ZM493.951 527.127H461.13V428.598H493.951V527.127Z" fill="#F7F7F5"/>
+        </g>
+        <g className="letter__part">
+          <path className="letter__layer color-2" 
+          d="M328.207 328.428V624.013H623.592V328.428H328.207ZM493.951 527.127H461.13V428.598H493.951V527.127Z" fill="#F7F7F5"/>
+        </g>
+      </g>
+
+      {/* M */}
+
+      <g className="letter letter--7">
+        <g className="letter__part">
+          <path className="letter__layer color-1" 
+          d="M855.873 328.428L805.034 394.294L752.849 328.428H656.406V624.013H722.047H722.507V525.386L755.212 624.013H853.428L886.133 525.386V624.013H886.15H951.791V328.428H855.873Z" fill="#67C05F"/>
+        </g>
+        <g className="letter__part">
+          <path className="letter__layer color-3" 
+          d="M855.873 328.428L805.034 394.294L752.849 328.428H656.406V624.013H722.047H722.507V525.386L755.212 624.013H853.428L886.133 525.386V624.013H886.15H951.791V328.428H855.873Z" fill="#67C05F"/>
+        </g>
+        <g className="letter__part">
+          <path className="letter__layer color-2" 
+          d="M855.873 328.428L805.034 394.294L752.849 328.428H656.406V624.013H722.047H722.507V525.386L755.212 624.013H853.428L886.133 525.386V624.013H886.15H951.791V328.428H855.873Z" fill="#67C05F"/>
+        </g>
+      </g>
+
+      {/* S */}
+
+      <g className="letter letter--8">
+        <g className="letter__part">
+          <path className="letter__layer color-1" 
+          d="M1280 394.113V328.428H984.617V525.485H1148.72V558.328H984.617V624.013H1280V426.956H1115.9V394.113H1280Z" fill="#F7F7F5"
+          />
+        </g>
+        <g className="letter__part">
+          <path className="letter__layer color-3" 
+          d="M1280 394.113V328.428H984.617V525.485H1148.72V558.328H984.617V624.013H1280V426.956H1115.9V394.113H1280Z" fill="#F7F7F5"
+          />
+        </g>
+        <g className="letter__part">
+          <path className="letter__layer color-2" 
+          d="M1280 394.113V328.428H984.617V525.485H1148.72V558.328H984.617V624.013H1280V426.956H1115.9V394.113H1280Z" fill="#F7F7F5"
+          />
+        </g>
+      </g>
+    </svg>
+  )
+}
+
+export default FilmDomsAnime
+
+{
+  /* <svg width="1281" height="626" viewBox="0 0 1281 626" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M623.592 78.8228V0H574.361H377.438H328.207V78.8228H377.438V216.763H328.207V295.586H377.438H574.361H623.592V216.763H574.361V78.8228H623.592Z" fill="#E96437"/>
+<path d="M295.385 78.8228V0H164.103H0V78.8228V131.371V210.194V295.586H164.103V210.194H262.564V131.371H164.103V78.8228H295.385Z" fill="#F7F7F5"/>
+<path d="M820.509 164.214V0H656.406V164.214V295.586H820.509H951.791V164.214H820.509Z" fill="#FFEB74"/>
+<path d="M1280 394.113V328.428H984.617V525.485H1148.72V558.328H984.617V624.013H1280V426.956H1115.9V394.113H1280Z" fill="#F7F7F5"/>
+<path d="M143.294 330.07H131.282H0V625.656H88.6154H111.59H143.294C227.167 625.656 295.385 559.363 295.385 477.863C295.385 396.364 227.151 330.07 143.294 330.07ZM119.795 527.127V428.599H152.615V527.127H119.795Z" fill="#A0B8FF"/>
+<path d="M328.207 328.428V624.013H623.592V328.428H328.207ZM493.951 527.127H461.13V428.598H493.951V527.127Z" fill="#F7F7F5"/>
+<path d="M1184.08 0L1133.24 65.8663L1081.06 0H984.617V295.586H1050.26H1050.72V196.959L1083.42 295.586H1181.64L1214.34 196.959V295.586H1214.36H1280V0H1184.08Z" fill="#E75B4E"/>
+<path d="M855.873 328.428L805.034 394.294L752.849 328.428H656.406V624.013H722.047H722.507V525.386L755.212 624.013H853.428L886.133 525.386V624.013H886.15H951.791V328.428H855.873Z" fill="#67C05F"/>
+</svg> */
+}
---
+++ components/views/Home/About/Section/index.ts
@@ -0,0 +1,2 @@
+export { default as CommonSection } from './CommonSection'
+export { default as TopSection } from './TopSection'
