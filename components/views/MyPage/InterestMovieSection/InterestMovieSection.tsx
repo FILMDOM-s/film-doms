e4879311d8733d5ac99ee2534f82d91a2410c648
chigomuh <tngusrldnjs1@gmail.com>
2023-05-11 22:13:57 +0900

    fix: 관심영화 최대 개수를 제한합니다

--- components/views/MyPage/InterestMovieSection/InterestMovieSection.tsx
+++ components/views/MyPage/InterestMovieSection/InterestMovieSection.tsx
@@ -1,19 +1,25 @@
 import styled from '@emotion/styled'
 import { colors, flexGap, font } from '@/styles/emotion'
 import { Divider, Tag } from '@/components/common'
+import { MAX_INTEREST_MOVIE_COUNT } from '../constants'
 
 interface Props {
   interestMovieList: User.InterestMovie[]
 }
 
 const InterestMovieSection = ({ interestMovieList }: Props) => {
+  const sliceInterestMovieList = interestMovieList.slice(
+    0,
+    MAX_INTEREST_MOVIE_COUNT
+  )
+
   return (
     <Container>
       <Divider color={colors.primary.orange} size={4} limit="24px" />
       <Title>관심영화</Title>
       <Wrapper>
         <Box>
-          {interestMovieList.map((movie, idx) => {
+          {sliceInterestMovieList.map((movie, idx) => {
             return (
               <Tag key={`interestMovie-${idx}`} shape="round">
                 {movie}
