10f2014f09564cc201e7897b19e9576a1dc091cf
chigomuh <tngusrldnjs1@gmail.com>
2023-05-20 23:21:42 +0900

    feat: Form Regex를 구성합니다

--- constants/auth/regex.ts
+++ constants/auth/regex.ts
@@ -1,7 +1,6 @@
 export const EMAIL_REGEX =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-export const PASSWORD_REGEX =
-  /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,14}$/
+export const PASSWORD_REGEX = /(?=.*[a-zA-Z])(?=.*[0-9]).{8,}/
 
-export const NICKNAME_REGEX = /^[가-힣|]{2,}$/
+export const NICKNAME_REGEX = /[a-zA-Zㄱ-ㅎ가-힣]{2,10}/
