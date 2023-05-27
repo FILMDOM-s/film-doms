7ea3ca459dc9c4b256e5494369c2a20a93ea6104
thewoowon <thewoowon@naver.com>
2023-05-27 18:24:16 +0900

    feat: editor 컴포넌트 수정

--- components/views/Article/Write/Check/LabeledCheckbox.tsx
+++ components/views/Article/Write/Check/LabeledCheckbox.tsx
@@ -1,10 +1,18 @@
 import { colors } from '@/styles/emotion'
 import styled from '@emotion/styled'
 
-const LabeledCheckbox = ({ label, ...props }: { label: string }) => {
+const LabeledCheckbox = ({
+  label,
+  register,
+  name,
+}: {
+  label: string
+  register: any
+  name: string
+}) => {
   return (
     <Container>
-      <Checkbox {...props} type={'checkbox'} />
+      <Checkbox type={'checkbox'} {...register(name)} name={name} />
       <Label>{label}</Label>
     </Container>
   )
@@ -15,12 +23,21 @@ const Container = styled.div`
 `
 
 const Checkbox = styled.input`
-  width: 20px;
-  height: 20px;
-  border: 2px solid ${colors.primary.black};
-  border-radius: 5px;
+  appearance: none;
   outline: none;
   cursor: pointer;
+  color: #f7f7f5;
+  background: transparent;
+  border: 2px solid ${colors.primary.black};
+  width: 18px;
+  height: 18px;
+  &:checked {
+    background-color: ${colors.primary.black};
+    background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='white' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M5.707 7.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L7 8.586 5.707 7.293z'/%3e%3c/svg%3e");
+    background-size: 150% 150%;
+    background-position: 50%;
+    background-repeat: no-repeat;
+  }
 `
 const Label = styled.label`
   color: ${colors.primary.black};
