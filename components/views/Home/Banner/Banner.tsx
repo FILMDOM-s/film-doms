bc804bb18ec5b15a564f12c7e6a299bc19355fe5
chigomuh <tngusrldnjs1@gmail.com>
2023-08-01 20:55:46 +0900

    fix: 메인 첫 배너 클릭 시 영화 게시판으로 이동합니다

--- components/views/Home/Banner/Banner.tsx
+++ components/views/Home/Banner/Banner.tsx
@@ -1,47 +1,24 @@
 import { useFetchBannerList } from '@/services/main'
 import { mainBannerImage } from '@/assets/images/banner'
 import Slider from './Slider'
+import { useRouter } from 'next/router'
 
 const Banner = () => {
+  const { push } = useRouter()
   const { data: bannerList } = useFetchBannerList()
 
-  const mainBannerList: Main.Banner[] = [
+  const mainBannerList: Array<{ onClick?: VoidFunction } & Main.Banner> = [
     {
       id: 0,
       type: '나를 공유하는 공간',
       title: 'FILMDOMS`s MOVIE',
       subtitle: '',
       image: mainBannerImage,
+      onClick: () => {
+        push('/article/movie')
+      },
     },
-    {
-      id: 0,
-      type: '나를 공유하는 공간',
-      title: 'FILMDOMS`s MOVIE',
-      subtitle: '',
-      image: mainBannerImage,
-    },
-    {
-      id: 0,
-      type: '나를 공유하는 공간',
-      title: 'FILMDOMS`s MOVIE',
-      subtitle: '',
-      image: mainBannerImage,
-    },
-    {
-      id: 0,
-      type: '나를 공유하는 공간',
-      title: 'FILMDOMS`s MOVIE',
-      subtitle: '',
-      image: mainBannerImage,
-    },
-    {
-      id: 0,
-      type: '나를 공유하는 공간',
-      title: 'FILMDOMS`s MOVIE',
-      subtitle: '',
-      image: mainBannerImage,
-    },
-    //...bannerList.slice(0, 3),
+    ...bannerList.slice(0, 3),
   ]
 
   return <Slider banners={mainBannerList} />
